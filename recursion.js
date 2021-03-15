09eec9d5aaba8806eedf349b5a000d022ac5dfa5
Tong Allan Zhao <allanzhao@tongs-air.attlocal.net>
2021-03-15 12:08:22 -0700

    Add recurisve and iterative solutions for fib sequence and reverse str

--- recursion.js
+++ recursion.js
@@ -22,3 +22,47 @@ function findFactorialIterative(num) {
   }
   return result;
 }
+
+
+//Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21.....
+
+
+//O(n)
+function fibonacciIterative(n) {
+  const fibonacciNums = [0, 1, 1];
+
+  for (let i = 3; i <=n; i++) {
+    fibonacciNums.push(fibonacciNums[i-1] + fibonacciNums[i-2]);
+  }
+
+  return fibonacciNums[n];
+}
+
+
+//O(2^n)
+function fibonacciRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+}
+
+function reverseStringIterative(str) {
+  let reversedStr = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+
+  return reversedStr;
+}
+
+function reverseStringRecursive(str) {
+  if (str.length === 1) {
+    return str;
+  }
+  return str[str.length -1] + reverseStringRecursive(str.slice(0, str.length - 1));
+}
