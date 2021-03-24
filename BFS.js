85aba847ac6377cfb61998fe88645f174cbe86b6
Tong Allan Zhao <allanzhao@tongs-air.attlocal.net>
2021-03-24 11:01:19 -0700

    Implement BFS recursively

--- BFS.js
+++ BFS.js
@@ -148,14 +148,36 @@ class BinarySearchTree {
 
     return list;
   }
+  breadthFirstSearchRecursive(queue, list) {
+    if (queue.length === 0) {
+      return list;
+    }
+
+    let currentNode = queue.shift();
+    list.push(currentNode);
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+
+    return this.breadthFirstSearchRecursive(queue,list);
+
+
+  }
 }
 
 const tree = new BinarySearchTree();
 
 tree.insert(9);
 tree.insert(4);
-tree.insert(20);
-tree.insert(1);
 tree.insert(6);
-tree.insert(15);
+tree.insert(20);
 tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+tree.breadthFirstSearch(); //returns [9, 4, 20, 1, 6, 15, 170];
+tree.breadthFirstSearchRecursive([tree.root], []); //returns [9, 4, 20, 1, 6, 15, 170];
